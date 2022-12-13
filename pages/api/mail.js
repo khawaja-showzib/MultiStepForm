6b44fe6d44a7c3c8779937738095bfbff2324ff1
khawaja-showzib <khawajashowzib22@gmail.com>
2022-12-13 12:29:03 +0500

    Final Commit

---
+++ pages/api/mail.js
@@ -0,0 +1,26 @@
+const mail = require('@sendgrid/mail')
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default function handler(req, res) {
+  const body = JSON.parse(req.body)
+
+const message = `
+Name: ${body.username}\r\n
+Address: ${body.address}\r\n
+TOC: ${body.toc}\r\n
+PP: ${body.pp}
+`;
+
+const data = {
+  to: 'khawaja.showzib@unit52.pk',
+  from: 'showzib22@gmail.com',
+  subject: "New SendGrd Practise Message",
+  text: message,
+  html: message.replace(/\r\n/g, '<br>')
+}
+
+mail.send(data)
+
+  res.status(200).json({ status: "ok" })
+}
